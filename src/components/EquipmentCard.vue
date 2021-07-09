e072e697176de22655b8b6ea26d3b55b7d47b216
Torie Brown <jatorietowers@gmail.com>
2021-07-08 21:25:31 -0500

    show more details

--- src/components/EquipmentCard.vue
+++ src/components/EquipmentCard.vue
@@ -2,13 +2,26 @@
   <v-card elevation="5" outlined>
     <v-card-title class="justify-center"> {{ equipment.manufacturer }} </v-card-title>
     <v-card-subtitle> {{ equipment.equipment_type }} </v-card-subtitle>
-    <v-card-text v-if="!equipment.equipment_photos">NO PHOTOS</v-card-text>
+    <v-card-text v-if="!equipment.equipment_photos">
+      NO PHOTOS
+    </v-card-text>
+    <v-btn elevation="2" @click="show = !show">Details</v-btn>
+    <v-card-text v-if="show">
+      <p> Description: {{ equipment.description}}</p>
+      <p> Serial Number: {{ equipment.serial_number }}</p>
+      <p> Model Number: {{ equipment.model_number}}</p>
+    </v-card-text>
   </v-card>
 </template>
 <script>
 export default {
   name: 'equipment-card',
-  props: ["equipment"]
+  props: ["equipment"],
+  data(){
+    return {
+      show: false
+    }
+  }
 }
 </script>
 <style scoped>

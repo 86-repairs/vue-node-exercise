c391e8a772f4a8ea7fda056def3341c7910f4ca5
Torie Brown <jatorietowers@gmail.com>
2021-07-08 20:18:09 -0500

    create equipment card, display equipment data on home page showing type and manufacturer

---
+++ src/components/EquipmentCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card elevation="5" outlined>
+    <v-card-title class="justify-center"> {{ manufacturer }} </v-card-title>
+    <v-card-subtitle> {{ equipment_type }} </v-card-subtitle>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'equipment-card',
+  props: ["manufacturer", "equipment_type"]
+}
+</script>
+<style scoped>
+  .v-card {
+    width: 400px;
+    padding: 10px;
+    margin: 5px;
+  }
+  .justify-center {
+    justify-content: center;
+  }
+
+</style>

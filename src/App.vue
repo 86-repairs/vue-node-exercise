cd514ed0be9eb16a695a3327b7a9c6ead123a1cf
Leland <leland.a.warren@gmail.com>
2021-07-18 19:08:13 -0700

    Adding cards to front-end based on API call

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,45 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <Home />
+    <Card
+            v-for="item in equipmentList"
+        :key="item.serial_number"
+        :equipment_type="item.equipment_type"
+        :manufacturer="item.manufacturer"
+      >
+
+    </Card>
   </div>
 </template>
 
 <script>
-import Home from "./components/Home.vue";
 
+import Home from "./components/Home.vue";
+import Card from "./components/Card.vue";
 export default {
   name: "App",
   components: {
     Home,
+    Card
   },
+  data() {
+    
+    let data = [];
+    return  {
+      equipmentList: data
+  }},
+  beforeMount(){
+    this.getData();
+  },
+  methods:{
+    async getData(){
+      let tenantUrl = 'http://localhost:8100';
+      const res = await fetch(`${tenantUrl}/equipment`);
+      const data = await res.json();
+      this.equipmentList = data;
+    }
+  }
+  
 };
 </script>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card"> Manufacturer: {{manufacturer}} <br/>
+    Equipment: {{equipment_type}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'equipment-card',
+  props: ["manufacturer", "equipment_type"]
+}
+</script>
+
+<style scoped>
+  .card {
+    width: 500px;
+    height: 100px;
+    border: 5px solid;
+  }
+</style>

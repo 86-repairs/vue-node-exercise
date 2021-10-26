9078b089c0d2bc65e533209056ae6581241be997
Justin Page <justin.page@icloud.com>
2021-10-26 11:05:13 -0700

    add changes per README

--- api/api.js
+++ api/api.js
@@ -1,10 +1,13 @@
 const cors = require("cors");
 const express = require("express");
+const bodyParser = require('body-parser');
 const Equipment = require("./equipmentData");
+const fs = require('fs')
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json())
 app.options("*", cors());
 
 app.get("/equipment", (req, res) => {
@@ -17,6 +20,40 @@ app.get("/equipment", (req, res) => {
       res.json(Equipment);
     }
   }, 800);
+});
+
+app.post("/search", (req, res) => {
+    const vendor = req.body.vendor 
+    const resString = Equipment.filter((equip) => equip.manufacturer == vendor)
+
+    if (!vendor || !resString) {
+      res.status(500).json({ error: "Incorrect search parameters, or search returned nothing." });
+    } else {
+      res.status(200).json(resString);
+    }
+});
+
+app.post("/submit-ticket", (req, res) => {
+    var matchFound = Equipment.some((sn) => sn.serial_number == req.body.equipmentSerial)
+
+    if(matchFound)
+    {
+      try {
+        let fileData = fs.readFileSync('C:\\Users\\justi\\Documents\\Tickets.txt').toString();
+        if(fileData.length < 1)
+        {
+          fileData = "[]"
+        }
+        let tickets = JSON.parse(fileData);
+        tickets.push(req.body)
+        const data = fs.writeFileSync('C:\\Users\\justi\\Documents\\Tickets.txt', JSON.stringify(tickets));
+        res.status(200).json({ Message: "Ticket Submitted!" });
+      } catch (err) {
+        res.status(500).json({ error: err });
+      }
+    } else {
+      res.status(500).json({ error: "Serial Number not found" });
+    }
 });
 
 const port = 8100;
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,54 +1,123 @@
 <template>
   <div class="hello">
-    <h2>Welcome to Vue frontend exercise!</h2>
-    <ul>
-      <a target="_blank" href="https://www.86repairs.com/"
-        >Here's a link to our official company site</a
-      >
-    </ul>
-    <ul>
-      <a target="_blank" href="https://www.youtube.com/watch?v=OimrN6dn7HA"
-        >Here's a video of our CEO being his charismatic self</a
-      >
-    </ul>
-    <ul>
-      <a
-        target="_blank"
-        href="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-        >Here's a cute picture of a cat</a
-      >
-    </ul>
-    <ul>
-      <a
-        target="blank"
-        href="https://www.google.com/search?q=how+to+google+things+better"
-      >
-        Here are some tips about how to be a better developer</a
-      >
-    </ul>
+    <h1>Equipment List</h1>
+    <body>
+      <div id="App"> 
+        <ul v-for="(equip, index) in filteredActive" :key="index" >
+          <li :class="{ card: !equip.manufacturer || !equip.equipment_type }" >
+              <h3> {{ equip.manufacturer }} / {{ equip.equipment_type }}</h3>
+              <p v-if="isShowingDetails(index)"> SN:{{ equip.serial_number }}</p>
+              <p v-if="isShowingDetails(index)"> MODEL:{{ equip.model_number }}</p>
+              <p v-if="isShowingDetails(index)"> ACTIVE:{{ equip.active }}</p>
+              <button class= "button" @click="toggleInfo(index)"> 
+                  <span v-if="isShowingDetails(index)">Hide Details</span>
+                  <span v-else>Show Details</span>
+              </button>
+              <button @click="deleteListItem(equip.serial_number)">
+                <img src="./assets/trash.png" alt="trashcan" width="20" height="20"/>
+              </button>
+          </li>
+        </ul>
+      </div>
+    </body>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "Home",
+  data() {
+    return {
+      equipmentData: [],
+      errorMessage: "",
+      showFullDetails: false
+    }
+  },
+  created() {
+    this.getAllData()
+  },
+  computed: {
+    filteredActive() {
+      return this.equipmentData.filter((equip) => equip.active)
+    }
+  },
+  methods: {
+     async getAllData() {
+      try {
+        const res = await fetch('http://localhost:8100/equipment');
+
+        if (!res.ok) {
+          const message = `An error has occured: ${res.status} - ${res.statusText}`;
+          throw new Error(message);
+        }
+
+        this.equipmentData = await res.json()
+      } 
+      catch (err) 
+      {
+        this.equipmentData = err.message;
+      }
+    },
+    toggleInfo(index) {
+      this.showFullDetails = index
+    },
+    isShowingDetails(index) {
+      return this.showFullDetails === index
+    },
+    deleteListItem(serialNumber) {
+      return this.equipmentData.find((sn) => sn.serial_number == serialNumber).active = false
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body {
+  max-width: 960px;
+  margin: 20px auto;
+}
 h2 {
   margin: 60px 0 0;
 }
-ul {
-  list-style-type: none;
+p, h3 {
+  margin : 0;
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  list-style-type: none;
+  background: #fff;
+  margin: 20px auto;
+  padding: 10px 20px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+ul {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  background-color: yellow;
 }
 a {
   color: #42b983;
 }
+.button{
+  background-color: #2ab3c5; /* Green */
+  border: none;
+  color: white;
+  padding: 12px 28px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+}
+
 </style>
